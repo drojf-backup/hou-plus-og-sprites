028dd9a309f1e5ff32fee9e8f848a3216bb01667
drojf <1249449+drojf@users.noreply.github.com>
2023-09-10 10:37:03 +1000

    Automatically generate archives

--- generate_ogsprites_folders_for_mod_final.py
+++ generate_ogsprites_folders_for_mod_final.py
@@ -6,6 +6,9 @@ from pathlib import Path
 import shutil
 import re
 import csv
+import subprocess
+import time
+import traceback
 
 def get_key_from_stem(stem):
     key = stem
@@ -14,6 +17,56 @@ def get_key_from_stem(stem):
         key = stem[:-1]
 
     return key
+
+def call(args, **kwargs):
+    print("running: {}".format(args))
+    retcode = subprocess.call(args, **kwargs)  # use shell on windows
+    if retcode != 0:
+        # don't print args here to avoid leaking secrets
+        raise Exception("ERROR: The last call() failed with retcode {retcode}")
+
+
+def tryRemoveTree(path):
+    attempts = 5
+    for i in range(attempts):
+        try:
+            if os.path.isdir(path):
+                shutil.rmtree(path)
+            else:
+                os.remove(path)
+            return
+
+        except FileNotFoundError:
+            return
+        except Exception:
+            print(f'Warning: Failed to remove "{path}" attempt {i}/{attempts}')
+            traceback.print_exc()
+
+        time.sleep(1)
+
+
+# From https://stackoverflow.com/questions/22680954/7zip-ultra-lzma2-compression
+def sevenZipMakeArchive(input_path, output_filename, cwd=None):
+    if cwd is None:
+        cwd = os.getcwd()
+
+    output_path = os.path.join(cwd, output_filename)
+
+    if os.path.exists(output_path):
+        if os.path.isdir(output_path):
+            raise Exception("Error: folder exists with same name as output 7z archive")
+        else:
+            print(f"!!! Removing {output_path}")
+            os.remove(output_path)
+
+    sevenzipwithoutinout = '7za a -t7z -m0=lzma2:d1024m -mx=9 -aoa -mfb=64 -md=512m -ms=on'.split()
+    sevenzipcommand = sevenzipwithoutinout + [output_filename, input_path]
+    print(f"Calling 7zip as {sevenzipcommand}")
+
+    if cwd is None:
+        call(sevenzipcommand)
+    else:
+        call(sevenzipcommand, cwd=cwd)
 
 current_mod_sprites_folder = 'current_mod_sprites'
 
@@ -102,4 +155,11 @@ for (source, dest) in copy_list:
     dest_parent = Path(dest).parent
     os.makedirs(dest_parent, exist_ok=True)
     shutil.copy(source, dest)
+
+
+for j in range(9):
+    i = j + 1
+    seven_zip_input_folder = f"HigurashiEp{i:02}_Data"
+    output_file = f"hou-ogsprites-ep{i:02}.7z"
+    sevenZipMakeArchive(seven_zip_input_folder, output_file, cwd=output_folder)
 
